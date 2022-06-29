4b2cb85b4560c9412d9b4fa1ab3a6af08711e7a5
Nauana Lima <limanauana@gmail.com>
2022-06-29 17:30:55 -0300

    Aula de strings e condicionais.

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCaracteres.java
@@ -0,0 +1,26 @@
+
+public class TestaCaracteres {
+
+		public static void main (String[] args) {
+			
+			// O tipo char guarda apenas um caracter
+			
+			char letra = 'a';
+			System.out.println(letra);
+			
+			char valor = 92;
+			System.out.println(valor);
+			
+			valor = (char) (valor + 1);
+			System.out.println(valor);
+			
+			String frase = "Alura Cursos Online de Tecnologia";
+			System.out.println(frase);
+			
+			frase = frase + 2022;
+			System.out.println(frase);
+			
+			String numero = "10001";
+			System.out.println(numero);
+		}
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional.java
@@ -0,0 +1,22 @@
+
+public class TestaCondicional {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Testando condicionais\n");
+		int idade = 17;
+		
+		if (idade >= 18) {
+			
+			System.out.println("Você tem mais de 18 anos.");
+			
+		}
+		
+		else {
+			
+			System.out.println("Você é menor de idade.");
+			
+		}
+	}
+
+}

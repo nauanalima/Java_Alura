d4c5d85147b8976677fcac3d4b53df1a35c8c079
Nauana Lima <limanauana@gmail.com>
2022-06-29 11:32:17 -0300

    Aprendendo a declarar variáveis.

---
+++ sintaxe-variaveis-e-fluxo/src/TestaVariaveis.java
@@ -0,0 +1,20 @@
+
+public class TestaVariaveis {
+
+	public static void main (String[] args) {
+		
+		System.out.println("Olá, novo teste!");
+		
+		int idade;
+		
+		/*Atribuição de variável*/
+		idade = 29;
+		
+		System.out.println("Nauana tem " + idade + " anos.");
+		
+		/*Neste caso não vai pular linha*/
+		System.out.print(idade);
+		
+	}
+	
+}
